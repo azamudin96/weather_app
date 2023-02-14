fe52fa0e3f551d45c97f80c60730d053b9580ce7
Azamudin96 <muhamadazamudin96@gmail.com>
2023-02-14 15:06:35 +0800

    add-location-permission

--- lib/weather/list.dart
+++ lib/weather/list.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+
 import 'package:collection/collection.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'package:weather_app/custom/weathercard.dart';
 import 'package:weather_app/model/weather.dart';
 import 'package:weather_app/util/colors.dart';
@@ -18,10 +22,118 @@ class _WeatherListScreenState extends State<WeatherListScreen> {
   final List<List<Data>> weatherList = [];
   bool isLoading = false;
 
+  bool servicestatus = false;
+  bool haspermission = false;
+  late LocationPermission permission;
+  late Position position;
+  String long = "", lat = "";
+  late StreamSubscription<Position> positionStream;
+
   @override
   void initState() {
     super.initState();
-    getForecast();
+    checkGps();
+  }
+
+  checkGps() async {
+    servicestatus = await Geolocator.isLocationServiceEnabled();
+    if (servicestatus) {
+      permission = await Geolocator.checkPermission();
+
+      if (permission == LocationPermission.denied) {
+        permission = await Geolocator.requestPermission();
+        if (permission == LocationPermission.denied) {
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: const Text('Location permissions are denied'),
+                    content:
+                        const Text('Location access needed. Please try again.'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => {
+                          checkGps(),
+                          Navigator.of(context).pop(),
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ));
+        } else if (permission == LocationPermission.deniedForever) {
+          showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                    title: const Text(
+                        'Location permissions are permanently denied'),
+                    content: const Text(
+                        'Location access needed. Go to settings to enable.'),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: () => {
+                          openAppSettings(),
+                          Navigator.of(context).pop(),
+                        },
+                        child: const Text('OK'),
+                      ),
+                    ],
+                  ));
+        } else {
+          haspermission = true;
+        }
+      } else {
+        haspermission = true;
+      }
+
+      if (haspermission) {
+        setState(() {
+          //refresh the UI
+        });
+
+        getLocation();
+      }
+    } else {
+      print("GPS Service is not enabled, turn on GPS location");
+    }
+
+    setState(() {
+      //refresh the UI
+    });
+  }
+
+  getLocation() async {
+    position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    print(position.longitude); //Output: 80.24599079
+    print(position.latitude); //Output: 29.6593457
+
+    long = position.longitude.toString();
+    lat = position.latitude.toString();
+
+    setState(() {
+      //refresh UI
+      getForecast();
+    });
+
+    LocationSettings locationSettings = LocationSettings(
+      accuracy: LocationAccuracy.high, //accuracy of the location data
+      distanceFilter: 100, //minimum distance (measured in meters) a
+      //device must move horizontally before an update event is generated;
+    );
+
+    StreamSubscription<Position> positionStream =
+        Geolocator.getPositionStream(locationSettings: locationSettings)
+            .listen((Position position) {
+      print(position.longitude); //Output: 80.24599079
+      print(position.latitude); //Output: 29.6593457
+
+      long = position.longitude.toString();
+      lat = position.latitude.toString();
+
+      setState(() {
+        //refresh UI on update
+        getForecast();
+      });
+    });
   }
 
   void getForecast() async {
@@ -30,7 +142,7 @@ class _WeatherListScreenState extends State<WeatherListScreen> {
       weatherList.clear();
     });
     var url =
-        'https://api.openweathermap.org/data/2.5/forecast?lat=6.124785&lon=102.254379&appid=021ea371d852bf973533abfa2cc5cf0b';
+        'https://api.openweathermap.org/data/2.5/forecast?lat=${position.latitude}&lon=${position.longitude}&appid=021ea371d852bf973533abfa2cc5cf0b';
     final response = await dio.get(url);
     final weather = WeatherResponse.fromJson(response.data);
     if (weather.cod == '200') {
@@ -58,7 +170,7 @@ class _WeatherListScreenState extends State<WeatherListScreen> {
           IconButton(
             icon: const Icon(Icons.refresh),
             onPressed: () {
-              getForecast();
+              checkGps();
             },
           ),
         ],
